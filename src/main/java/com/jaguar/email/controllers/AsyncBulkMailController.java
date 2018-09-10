38e22ee33509d5e153117a830676c94eb65c66f0
Ankit Verma <Ankit.verma@pearson.com>
2018-09-10 20:12:34 +0530

    Added basic Thread executor

--- src/main/java/com/jaguar/email/controllers/AsyncBulkMailController.java
+++ src/main/java/com/jaguar/email/controllers/AsyncBulkMailController.java
@@ -1,21 +1,28 @@
 package com.jaguar.email.controllers;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.jaguar.email.services.AsyncMailService;
 
+@RestController
 public class AsyncBulkMailController {
 	
 	@Autowired
 	private JavaMailSender sender;
 	
+	private static final int MYTHREADS = 30;
+	
 	@RequestMapping("/sendAsync")
 	public String sendAsyncMail() {
+		ExecutorService executor = Executors.newFixedThreadPool(MYTHREADS);
 		AsyncMailService asyncMailService = new AsyncMailService(sender);
-		Thread t = new Thread(asyncMailService);
-		t.start();
+		executor.execute(asyncMailService);
 		return "Mail Sent Success!";
 	}
 
